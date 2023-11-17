5abd766c48af5aec296bb5e3e953a2bc0b13ae97
mrGolbya <mr.golbya@gmail.com>
2023-11-17 16:37:06 +0300

    fix: fix style adaptiv

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 function openClosePopup(e){
   if(e.target.classList.contains('button')){
     document.querySelector('#popup').classList.remove('hidden');
+    document.body.style.overflow='hidden'
   }
- else if(e.target.classList.contains('popup-close') || e.target.classList.contains('popup-body')){
+ else if(e.target.classList.contains('popup-close') || e.target.classList.contains('popup')){
   document.querySelector('#popup').classList.add('hidden');
+  document.body.style.overflow='auto'
  }
 }
-
 document.addEventListener('click',openClosePopup);
