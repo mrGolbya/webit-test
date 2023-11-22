2491dee3c1891abd7b04d556bba00dfa03aab25d
mrGolbya <mr.golbya@gmail.com>
2023-11-22 09:19:28 +0300

    feat: remove content offset when opening a modal window

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
+let widthBody = document.body.clientWidth;
+
 function openClosePopup(e){
   if(e.target.classList.contains('button')){
     document.querySelector('#popup').classList.remove('hidden');
-    document.body.style.overflow='hidden'
+    document.body.style.overflow='hidden';
+    let widthBodyClick = document.body.clientWidth;
+    document.body.style.paddingRight = `${widthBodyClick-widthBody}px`;
+    console.log(widthBodyClick);
   }
  else if(e.target.classList.contains('popup-close') || e.target.classList.contains('popup')){
   document.querySelector('#popup').classList.add('hidden');
-  document.body.style.overflow='auto'
+  document.body.style.overflow='auto';
+  document.body.style.padding = '0';
  }
 }
 document.addEventListener('click',openClosePopup);
